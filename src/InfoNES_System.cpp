916ff95962e8e3c7d4fbb5cf2ad8bec69c0f67c3
Legend <me@liux.pro>
2022-10-16 03:39:49 +0800

    remove bmp write

--- src/InfoNES_System.cpp
+++ src/InfoNES_System.cpp
@@ -4,7 +4,6 @@
 #include "string.h"
 #include "stdint.h"
 #include "stdio.h"
-#include "bmp.h"
 
 extern uint8_t rom[];
 
--- src/bmp.h
+++
@@ -1,1 +0,0 @@
-void WriteBMP(unsigned char *img, const char *filename, int w, int h);
